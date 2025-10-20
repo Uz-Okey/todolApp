f76a43a818f9d39aef7911832cbc91da06d94045
Uzondu Okechukwu <uzonduokey30@gmail.com>
2025-10-20 21:22:26 +0100

    promise change

--- src/app/api/todo/[id]/route.ts
+++ src/app/api/todo/[id]/route.ts
@@ -1,5 +1,5 @@
 import { prisma } from "@/app/utils/db";
-
+import { NextRequest, NextResponse } from "next/server";
 
 export async function DELETE(_request:Request, {params}: {params: {id: string}}) {
 
@@ -20,22 +20,15 @@ export async function DELETE(_request:Request, {params}: {params: {id: string}})
 
 }
 
-export async function PATCH(request : Request, {params} : {params: {id: string}}){
-    try{
-        const {completed} = await request.json()
 
-        if(completed === undefined){
-return new Response("missign completed field", {status: 400})
-        }
 
-          const updateTodo = await prisma.todo.update({
-        where:{id : params.id},
-        data: {completed}
-    })
+export async function PATCH(request: NextRequest, context: { params: Promise<{ id: string }> }) {
+  const { id } = await context.params;
+  const body = await request.json();
 
-    return Response.json(updateTodo, {status: 200})
-    }catch(err){
-        console.error(err, 'error')
-    }
-  
-}
+  const updatedTodo = await prisma.todo.update({
+    where: { id },
+    data: body,
+  });
+
+  return NextResponse.json(updatedTodo);
