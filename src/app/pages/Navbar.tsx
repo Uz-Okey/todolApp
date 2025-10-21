c69477f92abf73fcf598af6bf1fd962424c18949
Uzondu Okechukwu <uzonduokey30@gmail.com>
2025-10-21 09:16:26 +0100

    navbar setting

--- src/app/pages/Navbar.tsx
+++ src/app/pages/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
 
   
     return (
-        <div className={`flex  duration-1000 transition-all ease-in-out p-2 md:p-8 justify-between items-center mx-auto`}>
+        <div className={`flex  duration-1000 sm:mt-3 mt-4 sm:mb-2 mb-15 transition-all ease-in-out p-2 md:p-8 justify-between items-center mx-auto`}>
             <div className="font-bold text-2xl">
                 <Link href='/'>
                     <h1>My <span className="font-serif text-amber-600">TODO</span></h1>
